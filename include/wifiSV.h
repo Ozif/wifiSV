f5a77580a1b6bcad60d9189b7bd7813804b0ccb7
Ozif <147888884+Ozif@users.noreply.github.com>
2025-06-04 18:39:58 +0800

    Add files via upload

---
+++ include/wifiSV.h
@@ -0,0 +1,44 @@
+#ifndef WIFISV_H
+#define WIFISV_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ArduinoJson.h> // 引入 ArduinoJson 库
+
+extern WebServer server; // 定义server为WebServer 开启web服务器
+extern String get[2];  // 定义color为String[2] 用来沟通main，在ESPGetColor函数调用前改变，可在ESPGetColor函数调里直接使用
+
+
+/* WiFi初始化; */
+/**
+ * @param ssid 网络名称;
+ * @param password 网络密码;
+ * @return 无返回值;
+ * @SerialPrint 输出ip用小程序连接;
+ */
+void wifi_init(const char *ssid, const char *password);
+
+/* API初始化 */
+/**
+ * @brief Web_open: 验证接口;                  //在init里调用
+ * 
+ * @brief Web_getdata: 颜色数据接口;            //在init里调用
+ * @param jsonDoc 颜色json数据;
+ * 
+ * @brief Web_putdata: 颜色数据传输接口;         //在init里调用
+ * @param ESPGetColor 颜色数据回调函数;（不用管，init函数会自动给它传回调函数）; //ESPGetColor为回调函数，方便main处理颜色数据
+ * 
+ * @brief Web_init: http初始化;
+ * @param jsonDoc 颜色json数据;                 //给Web_putdata函数用
+ * @param ESPGetColor 颜色数据回调函数; (给Web_putdata函数传);  //ESPGetColor为回调函数，方便main处理颜色数据
+ * 
+ * @brief Web_loop: 循环处理;
+ */
+void Web_open();
+void Web_getdata(JsonDocument &jsonDoc);
+void Web_putdata(void ESPGetColor());
+void Web_init(JsonDocument &jsonDoc, void ESPGetColor());
+void Web_loop();
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include "wifiSV.h"
+
+// WiFi信息
+const char *ssid = "507";
+const char *password = "507507507507";
+
+// 先创建个Json对象
+JsonDocument doc;
+
+// getcolor  回调函数，给Web_init传给Web_putdata，可以自己改
+void ESPGetColor()
+{
+  Serial.println(get[0]);
+  Serial.println(get[1]);
+}
+
+void setup()
+{
+  // 给Web_init传给Web_getdata，可以自己改
+  // 给Json对象赋值
+  const char *color = "{\"color\":[\"red\",\"green\",\"blue\",\"pink\"]}";
+  DeserializationError error = deserializeJson(doc, color);
+  if (error)
+  {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    return;
+  }
+  
+  wifi_init(ssid, password);
+  Web_init(doc, ESPGetColor);
+}
+
+void loop()
+{
+  Web_loop();
+}
